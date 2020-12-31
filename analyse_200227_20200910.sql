bd43fc9f53437bc7979daa2408ebef23a312367a
Lizardie <lizardie@gmail.com>
2020-12-31 02:48:15 +0100

    add analyse not ffinished

--- analyse_200227_20200910.sql
+++ analyse_200227_20200910.sql
@@ -3,7 +3,88 @@
 -----------------------------------------
 -----------------------------------------
 
+------------ check tables
+
+SELECT * FROM agency_20200910 LIMIT 3;
+SELECT * FROM calendar_dates_20200910 LIMIT 3;
+SELECT * FROM routes_20200910 LIMIT 3;
+SELECT * FROM stops_20200910 LIMIT 3;
+SELECT * FROM trips_20200910 LIMIT 3;
+SELECT * FROM calendar_20200910 LIMIT 3;
+SELECT * FROM feed_info_20200910 LIMIT 3;
+SELECT * FROM stop_times_20200910 LIMIT 3;
+SELECT * FROM transfers_20200910 LIMIT 3;
+
+SELECT * FROM mt_stop_times_20200910 LIMIT 3;
+SELECT * FROM mt_stop_shapes_20200910 LIMIT 3;
+
 ------------ generate study sample
+
+
+BEGIN;
+SET LOCAL work_mem = '100MB';
+
+DROP TABLE IF EXISTS mt_sample_20200910;
+CREATE TABLE mt_sample_20200910 AS
+
+SELECT row_number() OVER () AS gid
+ , b.service_id
+ , b.route_id
+ , b.trip_id
+ , c.stop_id
+ , c.stop_sequence
+ , c.departure_time
+ --, c.arrival_time
+ , c.departure_hour
+ --, c.pickup_type, c.drop_off_type
+ --, b.trip_headsign , b.trip_short_name
+ --, f.route_short_name, f.route_long_name
+ , f.agency_id
+ , b.datadate
+ --, b.gid gid_trips
+ --, a.gid gid_calendar_dates
+ --, c.gid gid_stop_times
+ --, e.gid gid_stops
+ --, f.gid gid_routes
+ --, g.gid gid_agency
+ , e.stop_name
+ , f.route_type
+ , g.agency_name
+ --, f.route_url
+ , d.trunc_long_3006, d.trunc_lat_3006 , d.geom_trunc::geometry(point,3006)
+ , d.long_3006, d.lat_3006 , d.geom::geometry(point,3006)
+ --, a.date calendar_date
+FROM trips_20200910 b
+INNER JOIN calendar_dates_20200910 a ON a.service_id = b.service_id
+INNER JOIN mt_stop_times_20200910 c ON  b.trip_id = c.trip_id
+INNER JOIN mt_stop_shapes_20200910 d ON c.stop_id = d.stop_id
+INNER JOIN stops_20200910 e ON c.stop_id = e.stop_id
+INNER JOIN routes_20200910 f ON b.route_id = f.route_id
+INNER JOIN agency_20200910 g ON f.agency_id = g.agency_id
+WHERE a.date::int = a.datadate::int
+ AND c.departure_time>= '07:00:00'
+ AND c.departure_time< '09:00:00'
+ --AND f.agency_id IN ('275','276','279')
+ --AND ( agency_name ~ 'SL' OR agency_name ~ 'Skånetrafiken ')
+--ORDER BY service_id,route_id,trip_id ,stop_id
+--LIMIT 6
+;--243695 rows
+
+COMMIT;   -- or ROLLBACK
+
+ALTER TABLE mt_sample_20200910 ADD CONSTRAINT mt_sample_20200910_pk PRIMARY KEY (gid);
+
+CREATE INDEX ON mt_sample_20200910 USING gist (geom_trunc);
+CREATE INDEX ON mt_sample_20200910 USING gist (geom);
+CREATE INDEX ON mt_sample_20200910 USING BRIN(departure_hour);
+
+
+SELECT * FROM mt_sample_20200910 ORDER BY service_id, route_id, trip_id , stop_id
+LIMIT 3;
+
+
+
+
 
 DROP TABLE IF EXISTS mt_trunc_stops_times_SL_20200910;
 CREATE TABLE mt_trunc_stops_times_SL_20200910 AS
@@ -20,13 +101,15 @@ INNER JOIN mt_stop_times_20200910 b USING (stop_id)
 INNER JOIN trips_20200910 USING (trip_id)
 INNER JOIN routes_20200910 USING (route_id)
 INNER JOIN agency_20200910 USING (agency_id)
-WHERE
-  agency_name ~ 'SL'
+WHERE --agency_name ~ 'Skånetrafiken'
+  --( agency_name ~ 'SL' OR agency_name ~ 'Skånetrafiken ')
+  agency_id='276'
   AND departure_time>= '07:00:00'
   AND departure_time< '09:00:00'
 ORDER BY geom_trunc,departure_time, trip_id,stop_sequence
---LIMIT 33
+LIMIT 33
 ;
+
 SELECT * FROM mt_trunc_stops_times_SL_20200910 LIMIT 3;
 
 CREATE INDEX ON mt_trunc_stops_times_SL_20200910 USING gist (geom_trunc);
